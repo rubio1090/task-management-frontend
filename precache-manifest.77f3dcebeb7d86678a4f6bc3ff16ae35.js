c5ba512446022f32f71ea8d20e9331a9778b0b7c
rubio1090 <rubio1090@users.noreply.github.com>
2022-07-25 22:27:35 +0000

    deploy: aa8f27d63d2e8a37d8aa58fb8139483647525130

---
+++ precache-manifest.77f3dcebeb7d86678a4f6bc3ff16ae35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e71c029af06d3cf75fb85f15875419",
+    "url": "https://rubio1090.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "8bb5ee2b1b07becccd20",
+    "url": "https://rubio1090.github.io/task-management-frontend/static/css/main.221589fe.chunk.css"
+  },
+  {
+    "revision": "f9e270e02a7b98932528",
+    "url": "https://rubio1090.github.io/task-management-frontend/static/js/2.968454d7.chunk.js"
+  },
+  {
+    "revision": "8bb5ee2b1b07becccd20",
+    "url": "https://rubio1090.github.io/task-management-frontend/static/js/main.646664f7.chunk.js"
+  },
+  {
+    "revision": "c54cf929ad164aaa33ae",
+    "url": "https://rubio1090.github.io/task-management-frontend/static/js/runtime~main.e4b07d41.js"
+  }
+]);
